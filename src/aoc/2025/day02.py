0d1b8b0836ed15f5bdb49b3a721defdb3edc7d56
Gid <gsmith.jemba2000@gmail.com>
2025-12-02 05:40:17 +0000

    Faster day 2

--- src/aoc/2025/day02.py
+++ src/aoc/2025/day02.py
@@ -1,6 +1,6 @@
 """https://adventofcode.com/2025/day/2"""
 
-from aoc_utils import get_input_data
+from aoc_utils import get_input_data, print_time_taken
 
 actual_input = get_input_data(2025, 2)
 
@@ -8,6 +8,7 @@ actual_input = get_input_data(2025, 2)
 example_input = """11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124"""
 
 
+@print_time_taken
 def solve(inputs: str):
     ranges = inputs.split(",")
 
@@ -15,16 +16,19 @@ def solve(inputs: str):
     for start_end in ranges:
         start, end = map(int, start_end.split("-"))
         for number in range(start, end + 1):
-            for num_chunks in range(2, len(str(end)) + 1):
-                str_num = str(number)
-                chunk_length, r = divmod(len(str_num), num_chunks)
+            str_num = str(number)
+            number_length = len(str_num)
+            for num_chunks in range(2, number_length + 1):
+                chunk_l, r = divmod(number_length, num_chunks)
                 if r != 0:
                     continue
-                chunks = [
-                    str_num[i : i + chunk_length]
-                    for i in range(0, len(str_num), chunk_length)
-                ]
-                if len(set(chunks)) == 1:
+                is_invalid = True
+                for i in range(0, number_length - chunk_l, chunk_l):
+                    next_i = i + chunk_l
+                    if str_num[i : i + chunk_l] != str_num[next_i : next_i + chunk_l]:
+                        is_invalid = False
+                        break
+                if is_invalid:
                     part2 += number
                     if num_chunks == 2:
                         part1 += number
