18da87bb336403cbe015d2df672056d74fda7d37
Gid <gsmith.jemba2000@gmail.com>
2025-12-07 05:12:51 +0000

    Day 7 - part 1

--- src/aoc/2025/day07.py
+++ src/aoc/2025/day07.py
@@ -5,16 +5,51 @@ from aoc_utils import get_input_data
 actual_input = get_input_data(2025, 7)
 
 
-example_input = """xxx"""
+example_input = """.......S.......
+...............
+.......^.......
+...............
+......^.^......
+...............
+.....^.^.^.....
+...............
+....^.^...^....
+...............
+...^.^...^.^...
+...............
+..^...^.....^..
+...............
+.^.^.^.^.^...^.
+..............."""
 
 
 def solve(inputs: str):
-    values = tuple(map(int, inputs.splitlines()))
+    splitters = set()
+    beams = set()
+    for y, row in enumerate(inputs.splitlines()):
+        for x, c in enumerate(row):
+            if c == "S":
+                beams.add((x, y))
+            if c == "^":
+                splitters.add((x, y))
+    max_y = y + 1
+    splits = 0
+    for y in range(max_y):
+        new_beams = set()
+        for beam in beams:
+            x = beam[0]
+            if (x, y) in splitters:
+                new_beams.add((x - 1, y + 1))
+                new_beams.add((x + 1, y + 1))
+                splits += 1
+            else:
+                new_beams.add((x, y + 1))
+        beams = new_beams
 
-    print(f"Part 1: {False}")
+    print(f"Part 1: {splits}")
     print(f"Part 2: {False}\n")
 
 
 if __name__ == "__main__":
     solve(example_input)
-    # solve(actual_input)
+    solve(actual_input)
